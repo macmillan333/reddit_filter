b15d3f73b416af1b80bc2c5fee70056820c8f2ee
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2020-04-19 16:55:22 -0700

    Options page. Doesn't work yet.

---
+++ options.js
@@ -0,0 +1,8 @@
+// Initialization
+
+
+
+// Saving
+document.getElementsByTagName("button")[0].addEventListener("click", function(event){
+    console.log(document.getElementsByTagName("textarea")[0].value);
+});
